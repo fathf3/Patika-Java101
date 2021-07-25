212c9f772a96c036442ac3d62705edebd9513996
fathf3 <fatih-348@hotmail.com>
2021-07-25 14:16:01 +0300

    odev

---
+++ Fibonacci2/src/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	        System.out.println(fibo(7));
+	    
+	   
+
+	}
+	 static int fibo(int n) {
+	        if (n == 1 || n == 2) {
+	            return 1;
+	        }
+	        return fibo(n - 1) + fibo(n - 2);
+	    }
+
+}
